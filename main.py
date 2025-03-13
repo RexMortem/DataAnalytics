316e1620a24b4e3e563603d72627294fc3580fab
RexMortem <68436000+RexMortem@users.noreply.github.com>
2025-03-13 13:21:04 +0000

    Removed file handling from example

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ from Regression import sumAbsoluteError, sumSquaredError
 from OutputManager import Output
 
 output = Output()
-fileOutput = Output(type="File")
 
 # Lagrangian 
 
-l1 = Lagrangian([6, 9, 15], [10, 15, 20], fileOutput)
-fileOutput.write(filePath="l1")
+l1 = Lagrangian([6, 9, 15], [10, 15, 20], output)
+output.write()
 l1.evaluate(12, output)
 output.write()
 
-l1exact = Lagrangian(valsToFractions([6, 9, 15]), valsToFractions([10, 15, 20]), fileOutput)
-fileOutput.write(filePath="l1exact")
+l1exact = Lagrangian(valsToFractions([6, 9, 15]), valsToFractions([10, 15, 20]), output)
+output.write()
 l1exact.evaluate(12, output)
 output.write()
 
