6f420fd7d969b871e2c4beb4ed021754b1f55e31
RexMortem <68436000+RexMortem@users.noreply.github.com>
2025-03-13 12:29:23 +0000

    cleaned up example

--- main.py
+++ main.py
@@ -9,22 +9,18 @@ fileOutput = Output(type="File")
 
 # Lagrangian 
 
-# l1 = Lagrangian([6, 9, 15], [10, 15, 20], fileOutput)
-# fileOutput.write(filePath="l1")
-# l1.evaluate(12, output)
-# output.write()
+l1 = Lagrangian([6, 9, 15], [10, 15, 20], fileOutput)
+fileOutput.write(filePath="l1")
+l1.evaluate(12, output)
+output.write()
 
-# l1exact = Lagrangian(valsToFractions([6, 9, 15]), valsToFractions([10, 15, 20]), fileOutput)
-# fileOutput.write(filePath="l1exact")
-# l1exact.evaluate(12, output)
-# output.write()
-
-# Newton Forward Difference
+l1exact = Lagrangian(valsToFractions([6, 9, 15]), valsToFractions([10, 15, 20]), fileOutput)
+fileOutput.write(filePath="l1exact")
+l1exact.evaluate(12, output)
+output.write()
 
 # APCA
 
 APCA([7, 5, 5, 3, 2, 4, 4, 6], N=3)
 X,Y = APCA([7, 5, 3, 3, 3, 4, 4, 6], N=3)
 print(sumSquaredError(X, [7, 5, 3, 3, 3, 4, 4, 6]))
-
-# Linear Least Squares
